562c91b04298c68add7ebd88e449ef0d29f4e2f2
Jeremy Fairbank <elpapapollo@gmail.com>
2017-12-31 21:29:58 -0500

    Remove redundant font family declarations

--- src/Styles.elm
+++ src/Styles.elm
@@ -51,14 +51,6 @@ mobileThreshold =
     820
 
 
-fontSourceSansPro : Style.Property class variation
-fontSourceSansPro =
-    Font.typeface
-        [ Font.font "Source Sans Pro"
-        , Font.sansSerif
-        ]
-
-
 responsiveChoice : Int -> ResponsiveChoiceConfig a -> a
 responsiveChoice current config =
     if current >= config.threshold then
@@ -80,16 +72,17 @@ stylesheet : WindowSize a -> StyleSheet Style variation
 stylesheet size =
     Style.styleSheet
         [ style Site
-            [ fontSourceSansPro ]
+            [ Font.typeface
+                [ Font.font "Source Sans Pro"
+                , Font.sansSerif
+                ]
+            ]
         , style Sidebar
             [ Color.background (rgb 241 241 241)
             , Font.size 20
-
-            -- , fontSourceSansPro
             ]
         , style ProgrammingElmSideAd
             [ Font.size 16
-            , fontSourceSansPro
             , prop "margin-bottom" "60px !important"
             , prop "margin-top" "auto !important"
             ]
@@ -102,7 +95,6 @@ stylesheet size =
         , style Brand
             [ Font.size 26
             , Font.bold
-            , fontSourceSansPro
             ]
         , style MobileMenu
             [ Color.background (rgb 241 241 241) ]
@@ -114,7 +106,6 @@ stylesheet size =
             [ Font.size 22 ]
         , style MainContent
             [ Font.size 20
-            , fontSourceSansPro
             , prop "overflow" <|
                 mobileResponsiveChoice size.width ( "auto", "visible" )
             ]
@@ -124,26 +115,22 @@ stylesheet size =
             [ Font.size <|
                 mobileResponsiveChoice size.width ( 80, 46 )
             , Font.light
-            , fontSourceSansPro
             ]
         , style HomeTagline
             [ Font.size <|
                 mobileResponsiveChoice size.width ( 40, 24 )
             , Font.italic
             , Font.light
-            , fontSourceSansPro
             ]
         , style TalksHeading
             [ Font.size 40
             , Font.bold
-            , fontSourceSansPro
             ]
         , style TalksConference
             [ Color.background (rgb 241 241 241) ]
         , style TalksConferenceName
             [ Font.size 26
             , Font.bold
-            , fontSourceSansPro
             ]
         , style TalksTalkLink
             [ Color.text blue
